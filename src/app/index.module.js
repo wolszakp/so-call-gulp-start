8fb6ca70598d796084eb59eed89a2cbc3423115c
p.wolszakiewicz@controlexpert.com <p.wolszakiewicz@controlexpert.com>
2016-05-02 21:22:21 -0700

    Init

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('gulpStart', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ngRoute', 'ui.bootstrap', 'toastr']);
+
+})();
